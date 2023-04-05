682a397aa351949467acaf31cdf31b3344e701c9
ramialawad <91923380+ramialawad@users.noreply.github.com>
2023-04-05 10:45:09 +1000

    added footer

---
+++ parking-app/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import './footer.css';
+export default function Footer() {
+  return (
+    <footer>
+      <span>
+        RJP
+        <br />
+        Copyright &copy; 2023{" "}
+      </span>
+    </footer>
+  );
+}
