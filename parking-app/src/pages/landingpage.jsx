8bad92533486d57280fd881a0cc1a5f7bc1f27e6
ramialawad <91923380+ramialawad@users.noreply.github.com>
2023-04-12 23:14:41 +1000

    added landing page, routing

---
+++ parking-app/src/pages/landingpage.jsx
@@ -0,0 +1,23 @@
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Login from "../components/Login";
+import React, { useState, useEffect } from 'react';
+
+export default function LandingPage() {
+      const [data, setData] = useState([{}]);
+  
+  useEffect(()=> {
+    fetch("/example").then(res => res.json()).then(res => setData(res)).catch(error => console.error(error))
+  }, [])
+  return (
+    <div>
+        <h1>Hello RJP</h1>
+      <h2>{data.name}</h2>
+       <h2>{data.message}</h2>
+    <h1>Sign up</h1> 
+     <Login/>       
+    </div>
+   
+  );
+}
